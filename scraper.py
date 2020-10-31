cb6e8dde899bb212d8315aca73b79604913645c2
Luke <lukas.zahoransky@zsbystricany.info>
2020-10-31 21:48:49 +0100

    Fixed formatting

--- scraper.py
+++ scraper.py
@@ -1,9 +1,7 @@
-#from urllib.request import urlopen
 import wget
 from selenium import webdriver
 from twitter_poster import post_to_twitter
-from selenium.webdriver.chrome.options import Options 
-import json
+from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()  
 chrome_options.add_argument("--headless") 
@@ -13,7 +11,7 @@ url = "https://www.youtube.com/c/KalleHallden/videos"
 browser.get(url)
 video = browser.find_element_by_xpath('//*[@id="video-title"]')
 video_title = str(video.text)
-F = open('titles.txt','r')
+F = open('titles.txt', 'r')
 past_title = F.readline()
 
 if past_title == video_title:
@@ -22,10 +20,10 @@ else:
 	video.click()
 
 	video_url = str(browser.current_url)
-	id = video_url.split("=",1)[1]
-	thumbnailurl= 'https://img.youtube.com/vi/'+id + '/maxresdefault.jpg'
+	video_id = video_url.split("=", 1)[1]
+	thumbnailurl = 'https://img.youtube.com/vi/' + video_id + '/maxresdefault.jpg'
 	thumbnail = wget.download(thumbnailurl)
 	image_path = 'maxresdefault.jpg'
 	post_to_twitter(image_path, video_title, video_url)
-	F = open('titles.txt','w')
+	F = open('titles.txt', 'w')
 	F.write(video_title)
--- twitter_poster.py
+++ twitter_poster.py
@@ -1,15 +1,11 @@
-
-
 import tweepy
 from tokens import getCreds
 
+
 def post_to_twitter(image_path, video_title, video_link):
 	creds = getCreds()
-
 	auth = tweepy.OAuthHandler(creds["consumer_key"], creds["consumer_secret"])
 	auth.set_access_token(creds["access_token"], creds["access_token_secret"])
 	api = tweepy.API(auth)
-
 	tweet_text = video_title + " " + video_link
-
 	status = api.update_with_media(image_path, tweet_text)
