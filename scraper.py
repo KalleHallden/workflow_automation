b8dc44bc0b35c192f5af4779de1b4f8a1ec5f9d3
Kalle Hallden <kallehalldeen@gmail.com>
2020-11-01 08:48:47 +0100

    Fixed some minor bugs

--- scraper.py
+++ scraper.py
@@ -4,6 +4,8 @@ from twitter_poster import post_to_twitter
 from selenium.webdriver.chrome.options import Options 
 import json
 import datetime
+import os
+import os.path
 
 
 chrome_options = Options()  
@@ -20,8 +22,8 @@ with open('data.json','r') as f:
 
 if past_title == video_title:
 	print("Don't post")
-	l_updated=date["last_updated"]
-	print(f"Last updated was {l_updated}")
+	l_updated=data["last_updated"]
+	print("Last updated was " + l_updated)
 else: 
 	video.click()
 
@@ -31,7 +33,16 @@ else:
 	thumbnail = wget.download(thumbnailurl)
 	image_path = 'maxresdefault.jpg'
 	post_to_twitter(image_path, video_title, video_url)
-  data["title"]=video_title
+
+	# move image into folder
+	image_num = data["image_num"]
+	working_dir = os.getcwd()
+	old_path = os.path.join(working_dir, image_path)
+	new_path = os.path.join(working_dir + "\Photos", str(image_num) + image_path)
+	os.rename(old_path, new_path)
+
+	data["title"]=video_title
 	data["last_updated"]=str(datetime.date.today())
+	data["image_num"] = + 1
 	with open("data.json","w") as f:
 		json.dump(data,f)
