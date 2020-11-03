21770b304ecb3ffe3e174e2ab904daa4d58f968c
Kalle Hallden <kallehalldeen@gmail.com>
2020-11-03 11:17:57 +0100

    removed __pychache__ folder

--- instagram_poster.py
+++ instagram_poster.py
@@ -4,9 +4,9 @@ from tokens import getCreds
 bot = Bot()
 
 
-def instagram_post(image_path, video_title, video_link):
+def instagram_post(image_path, video_title):
     creds = getCreds()
     bot.login(username=creds['username'],
               password=creds['password'])
-    caption_text = f'{video_title} {video_link}'
+    caption_text = f'{video_title}'
     bot.upload_photo(image_path, caption=caption_text)
--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ import json
 import datetime
 import os
 import os.path
+from instagram_poster import instagram_post
 
 
 chrome_options = Options()  
@@ -27,22 +28,43 @@ if past_title == video_title:
 else: 
 	video.click()
 
-	video_url = str(browser.current_url)
-	video_id = video_url.split("=", 1)[1]
+	video_link = str(browser.current_url)
+	video_id = video_link.split("=", 1)[1]
 	thumbnailurl = 'https://img.youtube.com/vi/' + video_id + '/maxresdefault.jpg'
 	thumbnail = wget.download(thumbnailurl)
 	image_path = 'maxresdefault.jpg'
-	post_to_twitter(image_path, video_title, video_url)
+
+	while True:
+		print("Do you want to post to Instagram? Y/N")
+		insta_input = str(input())
+		insta_input = insta_input.lower()
+		if insta_input != 'n' or 'y':
+			print("NOOO")
+			continue
+		else:
+			break
+	
+	while True:
+		print("Do you want to post to Twitter? Y/N")
+		twitter_input = str(input())
+		twitter_input = twitter_input.lower()
+		if twitter_input != 'n' or 'y':
+			continue
+		else:
+			break
+
+	if insta_input == 'y':
+		instagram_post(image_path, video_title)
+	if twitter_input == 'y':
+		post_to_twitter(image_path, video_title, video_link)
 
 	# move image into folder
-	image_num = data["image_num"]
 	working_dir = os.getcwd()
 	old_path = os.path.join(working_dir, image_path)
-	new_path = os.path.join(working_dir + "\Photos", str(image_num) + image_path)
+	new_path = os.path.join(working_dir + "\Photos", video_title)
 	os.rename(old_path, new_path)
 
 	data["title"]=video_title
 	data["last_updated"]=str(datetime.date.today())
-	data["image_num"] = + 1
 	with open("data.json","w") as f:
 		json.dump(data,f)
